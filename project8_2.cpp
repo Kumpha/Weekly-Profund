11ed34cd34120c256c079ad7a6b61859ea049c07
Kumpha <89091440+Kumpha@users.noreply.github.com>
2021-10-13 22:15:28 +0700

    Add files via upload

---
+++ project8_2.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+void whb(){
+	float weight, height,bmi;
+	printf("BMI Calculator");
+	printf("\nEnter your weight (kg) :");
+	scanf("%f", &weight);
+	printf("Enter your height (m) :");
+	scanf("%f", &height);
+	
+	bmi = weight / (height * height);
+	printf("Body mass index (BMI) is %.2f", bmi);
+
+ 	if(bmi>=0&&bmi<18.5)
+		printf("\nUnderweight",bmi);
+
+ 	if(bmi>=18.5&&bmi<24.9)
+		printf("\nNormal Weight", bmi);
+
+ 	if(bmi>=25&&bmi<29.9)
+		printf("\nOverweight", bmi);
+
+ 	if(bmi>30)
+		printf("\nObesity", bmi);
+}
+
+int main() {
+	whb();
+	
+	return 0;
+}
